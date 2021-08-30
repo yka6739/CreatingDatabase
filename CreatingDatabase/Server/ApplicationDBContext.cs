d8cdba51d2865b2614ccf2cfc21a3ed7ffdbf0fb
YATENDRA AWANA <yka6739@gmail.com>
2021-08-30 15:08:19 +0530

    Add project files.

---
+++ CreatingDatabase/Server/ApplicationDBContext.cs
@@ -0,0 +1,23 @@
+﻿using CreatingDatabase.Shared.Entities;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CreatingDatabase.Server
+{
+    public class ApplicationDBContext:DbContext
+    {
+        public ApplicationDBContext(DbContextOptions<ApplicationDBContext>options):base (options)
+        {
+
+
+        }
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<AddNewRecord>().HasKey(x => new { x.Id });      
+        }
+        public DbSet<AddNewRecord> NewRecords { get; set; }
+    }
+}
---
+++ CreatingDatabase/Shared/Entities/AddNewRecord.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CreatingDatabase.Shared.Entities
+{
+   public  class AddNewRecord
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string City { get; set; }
+        public string Course { get; set; }
+    }
+}
